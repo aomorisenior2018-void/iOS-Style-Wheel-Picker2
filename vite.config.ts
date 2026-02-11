6a38368c242762047ecef6711ef3ff25784574bf
aomorisenior2018-void <aomorisenior2018@gmail.com>
2026-02-11 09:33:35 +0900

    test2

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Important for GitHub Pages to load assets correctly from subdirectories
+  base: '/iOS-Style-Wheel-Picker2/', // Important for GitHub Pages to load assets correctly from subdirectories
 });
